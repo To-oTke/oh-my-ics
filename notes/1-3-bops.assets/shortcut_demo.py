63505658424d0fbc2959cd3b6f702faca4ec7737
yuetsin <akaza_akari@sjtu.edu.cn>
2020-02-28 12:11:59 +0800

    [add] binary operations phase

---
+++ notes/1-3-bops.assets/shortcut_demo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+def logical_and(left: Exp, right: Exp) -> int:
+    if left.eval() != 0:
+        if right.eval() != 0:
+            return 1
+        else:
+            return 0
+    else:
+        # right exp was short-cut
+        # when left.eval == 0
+        return 0
+
+
+def logical_or(left: Exp, right: Exp) -> int:
+    if left.eval() == 0:
+        if right.eval() == 0:
+            return 0
+        else:
+            return 1
+        else:
+            # right exp was short-cut
+            # when left.eval != 0
+        return 1
